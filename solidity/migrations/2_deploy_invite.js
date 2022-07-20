8ddfd872f11105f92f23c45170fc5d20076310cd
Chris Sutton <chris_sutton@hey.com>
2022-07-20 09:33:27 -0500

    fixes pr request changes for solidity

--- solidity/migrations/2_deploy_invite.js
+++ solidity/migrations/2_deploy_invite.js
@@ -1,5 +1,5 @@
 // migrations/2_deploy_invite.js
-const DiscordInvite = artifacts.require('DiscordInvite');
+const DiscordInvite = artifacts.require("DiscordInvite");
 
 module.exports = async function (deployer) {
   await deployer.deploy(DiscordInvite);
