ffcd978817deffa7087ce6dbdac2bc228120f0b3
Mateo Karadza <mateo.karadza@gmail.com>
2022-07-27 14:40:49 +0200

    poc for scheduled cron

---
+++ adapters/algorand.adapter.ts
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+export interface AlgorandAsset {
+  amount: number;
+  "asset-id": string;
+  deleted: boolean;
+  "is-frozen": boolean;
+}
+
+export interface AlgorandAccount {
+  address: string;
+  amount: number;
+  assets: AlgorandAsset[];
+}
+
+const ASA_ID = process.env.ASA_ID as string;
+const ALGORAND_NETWORK = process.env.ALGORAND_NETWORK as
+  | "mainnet"
+  | "testnet"
+  | "betanet";
+
+const AlgoExplorerIndexerURLs = {
+  mainnet: "https://algoindexer.algoexplorerapi.io/v2",
+  testnet: "https://algoindexer.testnet.algoexplorerapi.io/v2",
+  betanet: "https://algoindexer.betanet.algoexplorerapi.io/v2",
+};
+
+export default class AlgorandAdapter {
+  static instance: AlgorandAdapter;
+  static algoExplorerBaseUrl: string;
+  // prevents class being initialized with `new` syntax
+  private constructor() {}
+
+  static getInstance() {
+    if (!AlgorandAdapter.instance) {
+      AlgorandAdapter.instance = new AlgorandAdapter();
+
+      this.algoExplorerBaseUrl = AlgoExplorerIndexerURLs[
+        ALGORAND_NETWORK
+      ] as string;
+    }
+
+    return AlgorandAdapter.instance;
+  }
+
+  async getAccount(address: string) {
+    const result = await axios.get(
+      `${AlgorandAdapter.algoExplorerBaseUrl}/accounts/${address}`
+    );
+
+    if (result.status !== 200) {
+      throw new Error("failed to fetch account details");
+    }
+
+    const account = result.data.account as AlgorandAccount;
+
+    return account;
+  }
+}
---
+++ pages/api/scheduled/check-algorand-assets/index.ts
@@ -0,0 +1,79 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
+import AlgorandAdapter, {
+  AlgorandAccount,
+} from "../../../../adapters/algorand.adapter";
+
+const cors = Cors({
+  methods: ["GET"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+function verifyAccountHoldsToken(account: AlgorandAccount, assetId: string) {
+  const assets = account.assets;
+
+  if (assets.length === 0) return false;
+
+  let tokenFound = false;
+  assets.forEach((asset) => {
+    if (asset["asset-id"] === assetId && asset.amount > 0) {
+      tokenFound = true;
+    }
+  });
+
+  return tokenFound;
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  await runMiddleware(req, res, cors);
+
+  // const supabase = await SupabaseAdapter.getInstance();
+  // const discord = await DiscordAdapter.getInstance();
+
+  const algorandAdapter = AlgorandAdapter.getInstance();
+
+  const asaId = process.env.ASA_ID as string;
+
+  // For demo purposes only, this is bad approach!
+  // TODO: Fetch ALL Discord users that have an algorand asset used for the invite
+
+  // Some hardcoded address I found
+  const retrievedAddresses = [
+    "3TJBXERIY2UQSU7JCLHY7EYK7CWSI463I75UMEGUWFABQZMBVGRVIQUTSA",
+  ];
+
+  await Promise.all(
+    retrievedAddresses.map(async (address) => {
+      const account = await algorandAdapter.getAccount(address);
+
+      const tokenFound = verifyAccountHoldsToken(account, asaId);
+
+      if (!tokenFound) {
+        // TODO: remove entry from database
+        // TODO: remove from discord
+        // await discord.removeRole('memberId', ROLE_NAME)
+      }
+    })
+  );
+
+  return res.status(200);
+};
+
+export default handler;
